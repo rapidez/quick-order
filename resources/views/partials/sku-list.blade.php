15b542b4f3244c40022dfc332cee4a55bc094210
Roene Verbeek <85165259+Roene-JustBetter@users.noreply.github.com>
2026-03-20 10:31:38 +0100

    Tailwind 4 update (#3)

--- resources/views/partials/sku-list.blade.php
+++ resources/views/partials/sku-list.blade.php
@@ -1,4 +1,4 @@
-<div class="bg rounded p-5 flex flex-col flex-1">
+<div class="bg rounded-sm p-5 flex flex-col flex-1">
     <div class="font-bold mb-4">@lang('Add products individually')</div>
     <div v-bind:key="quickOrder.productCount">
         <div class="flex items-center gap-x-2 text-sm font-bold text-left mb-4">
@@ -12,7 +12,7 @@
                 @lang('Product options')
                 <span class="text-danger">*</span>
             </div>
-            <div class="w-[68px]"></div>
+            <div class="w-17"></div>
         </div>
         <div class="flex flex-col gap-4 min-h-56">
             <template v-for="product, productId in quickOrder.products">
